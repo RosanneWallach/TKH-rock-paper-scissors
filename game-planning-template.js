c2e69abd0f28a3870275f8413ea2183bcdc86bbc
Rosanne Wallach <Rosannewallach@gmail.com>
2022-11-02 22:11:35 -0400

    Updated game planning template

--- game-planning-template.js
+++ game-planning-template.js
@@ -1,10 +1,14 @@
-//Game title: 
+//Game title: Tic Tac Toe
 
  /*General Overview of Gameplay 
 Describe the sequence of events that happen in your game
 include how the score is used 
 include how the game will end - including win/lose message 
 
+Player vs computer
+Best 2/3 rounds
+Player will be player X and Computer will be O
+Player X has to beat Computer O
 
 */
 
@@ -13,25 +17,28 @@ include how the game will end - including win/lose message
 /*Requirements - include a short note about how your game will fulfill each of these requirements 
 
 user interaction causes changes to the web page using DOM methods (at least 3 changes during game play) can be images, text, score indicator 
-  -
+   -show images of player's and computer's assigned Tic Tac Toe
+    -display score of player and computer 
+    -display win or lose message
 
 correct use of at least 1 JavaScript Class that is used in the game 
-  -
+  -else, else if, func, getElementById
 
 use of a method defined in your class 
-  -
+  -Player will have a name property, score property, addToScore method
 
 keep score - user must be able to gain points
-  -
+  -addToScore method 
+  
 
 declare if the user won or lost at the end of the game and display on the page 
-  -
+   -either player or Computer will win, decided by score 
 
  Use of CSS to set colors, fonts and overall layout of the page
-  -
+  - FLEXBOX!!!
 
 use of iteration. examples: for loop, while loop, .forEach method, .map method
-  -
+  -still haven't decided fully but maybe .map method?
   
 */
 
@@ -39,6 +46,11 @@ use of iteration. examples: for loop, while loop, .forEach method, .map method
 
 /* Plan functions 
 start thinking about your basic functions! give them a name and list what they do 
+
+player is gonna be assigned X, unless I am able to make it possible for the player to choice either "X" or "O"
+player makes first move then the computer makes a move
+will be using if else statements, possibly nested statements
+possible use a while loop to keep score
 
 
 
